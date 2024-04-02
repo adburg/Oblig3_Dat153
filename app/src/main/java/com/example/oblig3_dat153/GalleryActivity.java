00c335d930555338e0951409775e398cafd648fa
amund.fremming <amund.fremming@gmail.com>
2024-04-02 19:10:44 +0200

    Back button func for Quiz and Gallery

--- app/src/main/java/com/example/oblig3_dat153/GalleryActivity.java
+++ app/src/main/java/com/example/oblig3_dat153/GalleryActivity.java
@@ -1,6 +1,9 @@
 package com.example.oblig3_dat153;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        Button saveButton = findViewById(R.id.button_save_exit);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GalleryActivity.this, MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/oblig3_dat153/QuizActivity.java
+++ app/src/main/java/com/example/oblig3_dat153/QuizActivity.java
@@ -1,6 +1,9 @@
 package com.example.oblig3_dat153;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Button homeButton = findViewById(R.id.button_home);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizActivity.this, MainActivity.class));
+            }
+        });
     }
 }
