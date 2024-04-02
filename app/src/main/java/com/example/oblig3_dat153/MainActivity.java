cd96d9159042ec87ce876f102ad89a09eaf6755c
adburg <adrian.berget@gmail.com>
2024-04-02 17:22:08 +0200

    not crashing

--- app/src/main/java/com/example/oblig3_dat153/MainActivity.java
+++ app/src/main/java/com/example/oblig3_dat153/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.oblig3_dat153;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
-    private final PhotoDAO dao = db.photoDAO();
+    private AppDatabase db;
+    private PhotoDAO dao;
+    private final Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
+        dao = db.photoDAO();
 
         handleGetAll();
         populateDb();
@@ -26,22 +33,28 @@ public class MainActivity extends AppCompatActivity {
     protected void handleGetAll() {
         Button btn = (Button) findViewById(R.id.test_button);
 
-        btn.setOnClickListener(v -> {
-            List<PhotoEntry> list = dao.getAll();
-            list.forEach(System.out::println);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<PhotoEntry> list = dao.getAll();
+                list.forEach(System.out::println);
+            }
         });
     }
 
     private void populateDb() {
-        PhotoEntry entry1 = new PhotoEntry("Entry1", "url1");
-        PhotoEntry entry2 = new PhotoEntry("Entry2", "url2");
-        PhotoEntry entry3 = new PhotoEntry("Entry3", "url3");
 
-        dao.insert(entry1);
-        dao.insert(entry2);
-        dao.insert(entry3);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                PhotoEntry entry1 = new PhotoEntry("Entry1", "url1");
+                PhotoEntry entry2 = new PhotoEntry("Entry2", "url2");
+                PhotoEntry entry3 = new PhotoEntry("Entry3", "url3");
+
+                dao.insert(entry1);
+                dao.insert(entry2);
+                dao.insert(entry3);
+            }
+        });
     }
-
-
-
 }
